41bc226cf45aa285955eeb93e6258b3c36731dc4
Benjamin Salmon <bensalmon8@gmail.com>
2022-07-09 15:28:34 -0400

    expense tracker

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
@@ -28,19 +30,24 @@ const App = () => {
     },
   ];
 
-  return React.createElement(
-    'div', 
-    {},
-    React.createElement('h2', {}, "Let's get started1"),
-    React.createElement(Expenses, {items: expenses})
-  );
-  
-  // return (
-  //   <div>
-  //     <h2>Let's get started!</h2>
-  //     <Expenses items={expenses}/>
-  //   </div>
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
+  // return React.createElement(
+  //   'div', 
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started1"),
+  //   React.createElement(Expenses, {items: expenses})
   // );
+  
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses}/>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
-import Card from './UI/Card';
+import Card from '../../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    // const expenseDate = new Date(2021, 2, 28);
-    // const expenseTitle = 'Car Insurance';
-    // const expenseAmount = 294.67;
-    // const month = props.date.toLocaleString('en-US', { month: 'long' });
-    // const day = props.date.toLocaleString('en-US', { day: '2-digit'});
-    // const year = props.date.getFullYear();
-
-    return (
-        <Card className='expense-item'>
-         <ExpenseDate date={props.date}/>
-         <div className='expense-item__desciption'>
-            <h2>{props.title}</h2>
-            <div className='expense-item__price'>${props.amount}</div>
-        </div>
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date}/>
+      <div className='expense-item__desciption'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
     </Card>
   );
 }
 
 export default ExpenseItem;
 
+
+// import React, { useState } from 'react';
+
+// import ExpenseDate from './ExpenseDate';
+// import Card from '../UI/Card';
+// import './ExpenseItem.css';
+
+// const ExpenseItem = (props) => {
+//   // function clickHandler() {}
+//   const [title, setTitle] = useState(props.title);
+//   console.log('ExpenseItem evaluated by React');
+  
+//   const clickHandler = () => {
+//     setTitle('Updated!');
+//     console.log(title);
+//   };
+
+//   return (
+//     <Card className='expense-item'>
+//       <ExpenseDate date={props.date} />
+//       <div className='expense-item__description'>
+//         <h2>{title}</h2>
+//         <div className='expense-item__price'>${props.amount}</div>
+//       </div>
+//       <button onClick={clickHandler}>Change Title</button>
+//     </Card>
+//   );
+// }
+
+// export default ExpenseItem;
